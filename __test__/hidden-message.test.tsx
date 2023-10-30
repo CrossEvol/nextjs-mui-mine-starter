6f38d8ff6be5d62f476c67abfec476f84051f0d8
创圣大天使Evol <296662402@qq.com>
2023-10-30 17:04:01 +0800

    1.0.22 Try to integrate with jest-dom and msw - jest.config.js - jest.setup.js - {render, screen, fireEvent} - rest, setupServer - import '@testing-library/jest-dom' - fireEvent.click() - screen.getByText() - fireEvent.change(ele,{}) - polyfill in jest.setup.js

---
+++ __test__/hidden-message.test.tsx
@@ -0,0 +1,21 @@
+import HiddenMessage from '@/app/test/_testing_library/HiddenMessage'
+import { fireEvent, render, screen } from '@testing-library/react'
+
+describe('HiddenMessageTest', () => {
+    it('shows the children when the checkbox is checked ', () => {
+        const testMessage = 'Test Message'
+        render(<HiddenMessage>{testMessage}</HiddenMessage>)
+
+        // query* functions will return the element or null if it cannot be found
+        // get* functions will return the element or throw an error if it cannot be found
+        expect(screen.queryByText(testMessage)).toBeNull()
+
+        // the queries can accept a regex to make your selectors more resilient to content tweaks and changes.
+        fireEvent.click(screen.getByLabelText(/show/i))
+
+        // .toBeInTheDocument() is an assertion that comes from jest-dom
+        // otherwise you could use .toBeDefined()
+        // expect(screen.getByText(testMessage)).toBeInTheDocument()
+        expect(screen.getByText(testMessage)).toBeInTheDocument()
+    })
+})
