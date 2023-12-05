a11cbc6d97b1af9669c1e9218b0f0e9de8c2e3da
创圣大天使Evol <296662402@qq.com>
2023-12-05 14:11:57 +0800

    remove /app/api/todos module

--- app/api/route.tsx
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    const data = 'hello, next api'
-
-    return NextResponse.json({ data })
-}
--- web/auth.ts
+++ web/auth.ts
@@ -1,7 +1,3 @@
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import { queryClient } from './client'
 import { mockApiUrl } from '@/config/url.config'
 
 export const logout = async () => {
