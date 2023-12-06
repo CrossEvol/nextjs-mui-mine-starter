0b35aa78e0371731093204d4b425e058baa2ec13
创圣大天使Evol <296662402@qq.com>
2023-12-06 12:18:41 +0800

    update and lock playwright to 1.40.0

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('auth', async ({ page }) => {
   await page.goto('http://localhost:3000/');
+  await page.screenshot({ path: 'e2e/screenshots/index.png' })
+
   await page.getByRole('link', { name: 'auth' }).click();
   await page.getByLabel('Email Address *').click();
   await page.getByLabel('Email Address *').fill('dfadsfsdaf');
   await page.getByLabel('Password *').click();
   await page.getByLabel('Password *').fill('fdsafdsfadsf');
   await page.getByLabel('Remember me').check();
+  await page.screenshot({ path: 'e2e/screenshots/login.png' })
   await page.getByRole('button', { name: 'Sign In' }).click();
+
   await page.getByRole('button', { name: '登 出' }).click();
+  await page.screenshot({ path: 'e2e/screenshots/login-success.png' })
   await page.getByRole('button', { name: '去登录' }).click();
+
   await page.getByRole('link', { name: 'Forgot password?' }).click();
+  await page.screenshot({ path: 'e2e/screenshots/back-to-login.png' })
   await page.getByRole('link', { name: 'Don\'t have an account? Sign Up' }).click();
+
+  await page.screenshot({ path: 'e2e/screenshots/register.png' })
   await page.getByLabel('First Name *').click();
   await page.getByLabel('First Name *').click();
   await page.getByLabel('First Name *').fill('dsafds');
@@ -21,7 +30,6 @@ test('auth', async ({ page }) => {
   await page.getByLabel('Email Address *').click();
   await page.getByLabel('Email Address *').fill('dsafds');
   await page.getByLabel('Password *').click();
-  await page.getByLabel('Password *').fill('fdsafds');
   await page.getByLabel('I want to receive inspiration, marketing promotions and updates via email.').check();
   await page.getByRole('button', { name: 'Sign Up' }).click();
   await page.getByRole('link', { name: 'Already have an account? Sign in' }).click();
